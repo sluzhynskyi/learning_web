2f0c17749fb92c1d9a21962495a133284cfa9721
Sluzhynskyi <sluzhynskyi@gmail.com>
2020-04-17 12:30:26 +0300

    docs reloading4

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,14 @@
 module.exports = eleventyConfig => {
     // Copy our static assets to the output folder
-    eleventyConfig.addPassthroughCopy('_components');
-    eleventyConfig.addPassthroughCopy('_includes');
+    eleventyConfig.addPassthroughCopy('components');
+    eleventyConfig.addPassthroughCopy('includes');
     // eleventyConfig.addPassthroughCopy('images');
 
     // Returning something from the configuration function is optional
     return {
         dir: {
-            input: '_inputs',
-            includes: "_includes",
+            input: 'inputs',
+            includes: "includes",
             output: "docs",
         }, pathPrefix: "/web_docs/"
     };
